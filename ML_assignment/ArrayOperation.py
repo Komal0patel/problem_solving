8dd78635e55bbd283446acf8127440d58f8a908e
Komal0patel <155667156+Komal0patel@users.noreply.github.com>
2025-03-07 00:22:01 +0530

    Create ArrayOperation.py
    performing basic operations on arrays, using numpy functions

---
+++ ML_assignment/ArrayOperation.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+
+array1=np.array([1,2,3,4])
+array2=np.array([6,7,8,9])
+res1= np.add(array1,array2)
+res2= np.subtract(array2,array1)
+res3= np.multiply(array1,array2)
+res4= np.divide(array2,array1)
+
+print(res1)
+print(res2)
+print(res3)
+print(res4)
